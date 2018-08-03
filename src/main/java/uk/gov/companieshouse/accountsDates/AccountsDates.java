1416489daf6ab73066c200fffbf54ce9e6d28423
leahtaylor <ltaylor@companieshouse.gov.uk>
2018-08-03 15:13:52 +0100

    Add accounts dates helper functionality and tests
    Refactor exisiting functionality to be clearer and use JAVA8 LocalDate rather than Date
    
    SFA-45

---
+++ src/main/java/uk/gov/companieshouse/accountsDates/AccountsDates.java
@@ -0,0 +1,188 @@
+package uk.gov.companieshouse.accountsDates;
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.Period;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
+import java.time.temporal.ChronoUnit;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AccountsDates {
+
+	private static final String PERIOD_START = "periodStart";
+	private static final String PERIOD_END = "periodEnd";
+	private static final String DATE_FORMAT_YYYYMMDD = "yyyy-MM-dd";
+	private static final String DATE_FORMAT_D_MMMM_YYYY = "d MMMM yyyy";
+
+	public LocalDate convertStringToDate(String stringDate) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT_YYYYMMDD);
+		return LocalDate.parse(stringDate, formatter);
+	}
+
+	public String convertDateToString(LocalDate date) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMAT_YYYYMMDD);
+		return date.format(formatter);
+	}
+
+	// from 2017-03-05 to 3 March 2005
+	public String convertStringToDisplayDate(String stringDate) {
+
+		DateTimeFormatter dateFormatOriginal = DateTimeFormatter.ofPattern(DATE_FORMAT_YYYYMMDD);
+		DateTimeFormatter dateFormatDesired = DateTimeFormatter.ofPattern(DATE_FORMAT_D_MMMM_YYYY);
+		LocalDate date = LocalDate.parse(stringDate, dateFormatOriginal);
+
+		return date.format(dateFormatDesired);
+	}
+
+	public Map<String, String> getDateAndTime(String dateString) {
+		Map<String, String> timeObject = new HashMap<>();
+
+		Instant instant = Instant.parse(dateString);
+
+		LocalDateTime localDateTime = LocalDateTime.ofInstant(instant, ZoneId.of("GMT"));
+		LocalDate localDate = localDateTime.toLocalDate();
+
+		DateTimeFormatter dateFormatDesired = DateTimeFormatter.ofPattern(DATE_FORMAT_D_MMMM_YYYY);
+		DateTimeFormatter timeFormatDesired = DateTimeFormatter.ofPattern("h:mm a");
+
+		String date = localDate.format(dateFormatDesired);
+		String time = localDateTime.format(timeFormatDesired).toLowerCase();
+
+		timeObject.put("date", date);
+		timeObject.put("time", time);
+
+		return timeObject;
+	}
+
+	/**
+	 * @param periodStart
+	 * @param periodEnd
+	 * @param isSameYear
+	 * @return
+	 * 
+	 * 		Generates balance sheet dates headings depending on range between
+	 *         period start and end dates
+	 * 
+	 */
+	public Map<String, String> calculatePeriodRange(LocalDate periodStart, LocalDate periodEnd, boolean isSameYear) {
+
+		int allowance = 15; // We allow year +/-15 days difference between dates to treat them as full year difference
+		int yearRangeMax = 365 + allowance;
+		int yearRangeMin = 365 - allowance;
+
+		Map<String, String> periodObject = new HashMap<>();
+
+		// period returns years, hours and days between dates eg 1 year, 3 months and 4 days
+		Period accountsPeriod = Period.between(periodStart, periodEnd);
+
+		long totalDaysDiff = ChronoUnit.DAYS.between(periodStart, periodEnd) + 1; // + 1 as they have time till the end of the day
+		long totalMonthsDiff = ChronoUnit.MONTHS.between(periodStart, periodEnd);
+
+		// If the days over an exact month are 1 to 15, then round down (unless thegperiod is less than 1 month, in which case round up).
+		// If the days over are 16 to 31, then round up. e.g. 15 months and 4 days is
+		// shown as 15 months; 15 months and 21 days is shown as 16 months.
+		totalMonthsDiff = (accountsPeriod.getDays() >= 15) ? totalMonthsDiff + 1 : totalMonthsDiff; // + 1 to days to include end date in calculation
+
+		// If the previous and current periods both end in the same year, then the
+		// heading is output as a full date e.g. ‘5 January 2015’ ’31 December 2015’.
+		if (isSameYear) {
+			periodObject.put(PERIOD_START, null);
+			periodObject.put(PERIOD_END, convertStringToDisplayDate(convertDateToString(periodEnd)));
+		}
+
+		// If the accounting period is twelve months (+/- 15 days - period start to
+		// period end)
+		// Then the heading for the balance sheet figures is: ccyy e.g. 2015 2016
+		else if ((totalDaysDiff >= yearRangeMin && totalDaysDiff <= yearRangeMax) && totalMonthsDiff == 12) {
+
+//		else if ((accountsPeriod.getYears()==1 && (accountsPeriod.getDays()<=15)) || (accountsPeriod.getMonths()==11 && (accountsPeriod.getDays()>=15))){
+			periodObject.put(PERIOD_START, null);
+			DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy");
+			periodObject.put(PERIOD_END, periodEnd.format(formatter));
+		}
+
+		// If the accounting period is less than twelve months (<350 days - period start
+		// to period end) or
+		// more than twelve months (>380 days - period start to period end)
+		// Then the heading for the balance sheet figures is 'x' months to dd/mm/ccyy
+		// e.g. 15 months to 31/12/2016
+		else if (totalDaysDiff < yearRangeMin || totalDaysDiff > yearRangeMax) {
+			String monthsDiffString = (totalMonthsDiff == 1) ? " month" : " months";
+			periodObject.put(PERIOD_START, totalMonthsDiff + monthsDiffString);
+			periodObject.put(PERIOD_END, convertStringToDisplayDate(convertDateToString(periodEnd)));
+		}
+
+		return periodObject;
+	}
+
+	public boolean isSameYear(LocalDate date1, LocalDate date2) {
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy");
+		String stringDate1 = date1.format(formatter);
+		String stringDate2 = date2.format(formatter);
+
+		return stringDate1.equals(stringDate2);
+	}
+
+	/**
+	 * Takes a {@link Date} and converts it to a Java 8 {@link LocalDate}
+	 * 
+	 * @param date
+	 * @return {@link LocalDate}
+	 */
+
+	public LocalDate convertDateToLocalDate(Date date) {
+		return date != null ? date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate() : null;
+	}
+
+	public List<LocalDate> getPreviousDays(LocalDate currentDate, int numDays) {
+		List<LocalDate> previousDays = new ArrayList<>();
+
+		for (int i = 1; i <= numDays; i++) {
+			previousDays.add(currentDate.minusDays(i));
+		}
+
+		return previousDays;
+	}
+
+	public List<LocalDate> getFutureDays(LocalDate currentDate, int numDays) {
+		List<LocalDate> futureDays = new ArrayList<>();
+		LocalDate now = LocalDate.now();
+		LocalDate nextDate;
+
+		for (int i = 1; i <= numDays; i++) {
+			nextDate = currentDate.plusDays(i);
+			if (nextDate.isAfter(now)) {
+				break;
+			}
+			futureDays.add(nextDate);
+		}
+
+		return futureDays;
+	}
+
+	public String generateBalanceSheetHeading(String periodStartString, String periodEndString, boolean isSameYear) {
+
+		Instant instantPeriodStart = Instant.parse(periodStartString);
+		Instant instantPeriodEnd = Instant.parse(periodEndString);
+
+		LocalDateTime localDateTimePeriodStart = LocalDateTime.ofInstant(instantPeriodStart, ZoneId.of("GMT"));
+		LocalDate localDatePeriodStart = localDateTimePeriodStart.toLocalDate();
+
+		LocalDateTime localDateTimePeriodEnd = LocalDateTime.ofInstant(instantPeriodEnd, ZoneId.of("GMT"));
+		LocalDate localDatePeriodEnd = localDateTimePeriodEnd.toLocalDate();
+
+		Map<String, String> resultdates = calculatePeriodRange(localDatePeriodStart, localDatePeriodEnd, isSameYear);
+
+		if (resultdates.get("periodStart") == null) {
+			return resultdates.get("periodEnd");
+		} else {
+			return resultdates.get("periodStart") + " to " + resultdates.get("periodEnd");
+		}
+	}
+}
---
+++ src/test/java/uk/gov/companieshouse/accountsDates/accountsDatesTest.java
@@ -0,0 +1,274 @@
+package uk.gov.companieshouse.accountsDates;
+
+import java.text.ParseException;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.junit.Test;
+
+import uk.gov.companieshouse.accountsDates.AccountsDates;
+
+public class accountsDatesTest {
+
+	private static final String YYYY_MM_DD = "yyyy-MM-dd";
+	private static final String PERIOD_START = "periodStart";
+	private static final String PERIOD_END = "periodEnd";
+
+	private SimpleDateFormat simpleDateFormat;
+	private AccountsDates datesHelper = new AccountsDates();
+
+	@Test
+	public void convertStringtoDate() throws ParseException {
+
+		LocalDate testDate = LocalDate.parse("2017-03-05");
+		LocalDate date = datesHelper.convertStringToDate("2017-03-05");
+
+		assertEquals(testDate, date);
+
+		testDate = LocalDate.parse("2013-01-22");
+		date = datesHelper.convertStringToDate("2013-01-22");
+		assertEquals(testDate, date);
+
+	}
+
+	@Test
+	public void convertStringtoDisplayDate() {
+
+		String date = datesHelper.convertStringToDisplayDate("2017-03-05");
+		assertEquals("5 March 2017", date);
+
+		date = datesHelper.convertStringToDisplayDate("2010-11-12");
+		assertEquals("12 November 2010", date);
+
+		date = datesHelper.convertStringToDisplayDate("2015-02-27");
+		assertEquals("27 February 2015", date);
+
+	}
+
+	@Test
+	public void getDateAndTime() {
+
+		String dateString = "2017-12-31T18:15:00.000Z";
+		Map dateMap = datesHelper.getDateAndTime(dateString);
+		assertEquals("31 December 2017", dateMap.get("date"));
+		assertEquals("6:15 pm", dateMap.get("time"));
+
+		dateString = "2016-12-02T03:15:22Z";
+		dateMap = datesHelper.getDateAndTime(dateString);
+		assertEquals("2 December 2016", dateMap.get("date"));
+		assertEquals("3:15 am", dateMap.get("time"));
+
+	}
+
+	@Test
+	public void convertDatetoString() throws ParseException {
+
+		DateTimeFormatter formatter = DateTimeFormatter.ofPattern(YYYY_MM_DD);
+
+		LocalDate testDate = LocalDate.parse("2017-03-05", formatter);
+		String stringDate = datesHelper.convertDateToString(testDate);
+		assertEquals("2017-03-05", stringDate);
+
+		LocalDate testDate2 = LocalDate.parse("2015-12-17", formatter);
+		String stringDate2 = datesHelper.convertDateToString(testDate2);
+		assertEquals("2015-12-17", stringDate2);
+
+	}
+
+	@Test
+	public void calculatePeriodRangeOver2Years() throws ParseException {
+
+		// Days difference over 2 years
+		LocalDate testDateStart = LocalDate.parse("2013-03-07");
+		LocalDate testDateEnd = LocalDate.parse("2017-12-17");
+		Map<String, String> periodMap = datesHelper.calculatePeriodRange(testDateStart, testDateEnd, false);
+		assertEquals("57 months", periodMap.get(PERIOD_START));
+		assertEquals("17 December 2017", periodMap.get(PERIOD_END));
+	}
+
+	@Test
+	public void calculatePeriodRangeOver380() throws ParseException {
+
+		// Days difference over 380
+		LocalDate testDateStart = LocalDate.parse("2015-03-07");
+		LocalDate testDateEnd = LocalDate.parse("2017-12-17");
+		Map<String, String> periodMap = datesHelper.calculatePeriodRange(testDateStart, testDateEnd, false);
+		assertEquals("33 months", periodMap.get(PERIOD_START));
+		assertEquals("17 December 2017", periodMap.get(PERIOD_END));
+	}
+
+	@Test
+	public void calculatePeriodRangeBelow350() throws ParseException {
+
+		// Days difference below 350
+		LocalDate testDateStart = LocalDate.parse("2016-08-03");
+		LocalDate testDateEnd = LocalDate.parse("2017-02-16");
+		Map<String, String> periodMap = datesHelper.calculatePeriodRange(testDateStart, testDateEnd, false);
+		assertEquals("6 months", periodMap.get(PERIOD_START));
+		assertEquals("16 February 2017", periodMap.get(PERIOD_END));
+	}
+
+	@Test
+	public void calculatePeriodLessThanOneMonth() throws ParseException {
+
+		// Month difference less than one month
+		LocalDate testDateStart = LocalDate.parse("2015-03-07");
+		LocalDate testDateEnd = LocalDate.parse("2015-04-01");
+		Map<String, String> periodMap = datesHelper.calculatePeriodRange(testDateStart, testDateEnd, false);
+		assertEquals("1 month", periodMap.get(PERIOD_START));
+		assertEquals("1 April 2015", periodMap.get(PERIOD_END));
+	}
+
+	@Test
+	public void calculatePeriodRangeBelow350RoundUp() throws ParseException {
+
+		// Days difference below 350 with month round up ( remaining days > 15 )
+		LocalDate testDateStart = LocalDate.parse("2016-08-01");
+		LocalDate testDateEnd = LocalDate.parse("2017-02-17");
+		Map<String, String> periodMap = datesHelper.calculatePeriodRange(testDateStart, testDateEnd, false);
+		assertEquals("7 months", periodMap.get(PERIOD_START));
+		assertEquals("17 February 2017", periodMap.get(PERIOD_END));
+	}
+
+	@Test
+	public void calculatePeriodRangeSameYear() throws ParseException {
+		// Same year
+		LocalDate testDateEndLast = LocalDate.parse("2017-08-12");
+		LocalDate testDateEndNext = LocalDate.parse("2017-11-23");
+		Map<String, String> periodMap = datesHelper.calculatePeriodRange(testDateEndLast, testDateEndNext, true);
+		assertEquals(null, periodMap.get(PERIOD_START));
+		assertEquals("23 November 2017", periodMap.get(PERIOD_END));
+	}
+
+	@Test
+	public void calculatePeriod12Months() throws ParseException {
+		// 12 months acc period ( +/- 15 days )
+		LocalDate testDateStart = LocalDate.parse("2016-08-12");
+
+		System.out.println("periodStartDate: " + testDateStart);
+
+		LocalDate testDateEnd = LocalDate.parse("2017-08-23");
+
+		System.out.println("periodEndDate: " + testDateStart);
+		Map<String, String> periodMap = datesHelper.calculatePeriodRange(testDateStart, testDateEnd, false);
+		assertEquals(null, periodMap.get(PERIOD_START));
+		assertEquals("2017", periodMap.get(PERIOD_END));
+
+	}
+
+	@Test
+	public void convertDateToLocalDate() throws ParseException {
+
+		simpleDateFormat = new SimpleDateFormat(YYYY_MM_DD);
+
+		Date date = simpleDateFormat.parse("2017-03-05");
+		LocalDate localDate = datesHelper.convertDateToLocalDate(date);
+		assertEquals("2017-03-05", localDate.toString());
+	}
+
+	@Test
+	public void convertStringToLocalDate() {
+
+		String date = "2017-03-05";
+		LocalDate localDate = datesHelper.convertStringToDate(date);
+		assertEquals("2017-03-05", localDate.toString());
+	}
+
+	@Test
+	public void getPreviousDays() {
+
+		int numDays = 3;
+
+		LocalDate localDate = LocalDate.of(2017, 1, 1);
+		List<LocalDate> previousDays = datesHelper.getPreviousDays(localDate, numDays);
+		List<LocalDate> testPreviousDays = new ArrayList<>();
+
+		for (int i = 1; i <= numDays; i++) {
+			testPreviousDays.add(localDate.minusDays(i));
+		}
+
+		assertEquals(testPreviousDays, previousDays);
+	}
+
+	@Test
+	public void getFutureDays() {
+
+		final int numDays = 7;
+
+		for (int i = 0; i < 9; i++) {
+			getFutureDays(numDays, i);
+		}
+	}
+
+	private void getFutureDays(int numberOfDays, int daysDifference) {
+		LocalDate now = LocalDate.now();
+		LocalDate localDate = now.minusDays(daysDifference);
+		List<LocalDate> futureDays = datesHelper.getFutureDays(localDate, numberOfDays);
+		List<LocalDate> testFutureDays = new ArrayList<>();
+
+		int numberOfFutureDays = Math.min(daysDifference, numberOfDays);
+
+		for (int i = 1; i <= numberOfFutureDays; i++) {
+			testFutureDays.add(localDate.plusDays(i));
+		}
+
+		assertEquals(testFutureDays, futureDays);
+	}
+
+	@Test
+	public void getFutureDatesLeapYear() {
+
+		final LocalDate startDate = LocalDate.of(2016, 02, 26);
+		final int numberOfDays = 7;
+
+		List<LocalDate> futureDays = datesHelper.getFutureDays(startDate, numberOfDays);
+
+		assertTrue(futureDays.contains(LocalDate.of(2016, 02, 29)));
+	}
+
+	@Test
+	public void generateCorrectBalanceSheetHeader() {
+
+		// test yyyy returned for 12 month period
+		assertEquals("2017",
+				datesHelper.generateBalanceSheetHeading("2016-01-01T00:00:00.000Z", "2017-01-14T00:00:00.000Z", false));
+
+		// Test 381 days shows month (more than 12 month period)
+		assertEquals("13 months to 16 February 2016",
+				datesHelper.generateBalanceSheetHeading("2015-02-01T00:00:00.000Z", "2016-02-16T00:00:00.000Z", false));
+
+		// Test 336 days shows month (less than 12 month period)
+		assertEquals("11 months to 1 July 2016",
+				datesHelper.generateBalanceSheetHeading("2015-08-01T00:00:00.000Z", "2016-07-01T00:00:00.000Z", false));
+
+		// Test exactly 381 days shows months leap year
+		assertEquals("13 months to 16 February 2015",
+				datesHelper.generateBalanceSheetHeading("2014-02-01T00:00:00.000Z", "2015-02-16T00:00:00.000Z", false));
+		
+		// Test 336 days shows 'month' rather than 'months'
+		assertEquals("1 month to 1 April 2015",
+				datesHelper.generateBalanceSheetHeading("2015-03-07T00:00:00.000Z", "2015-04-01T00:00:00.000Z", false));
+
+		// "Test exactly 351 days show yyyy"
+		assertEquals("2015",
+				datesHelper.generateBalanceSheetHeading("2014-04-01T00:00:00.000Z", "2015-03-16T00:00:00.000Z", false));
+
+		// Test exactly 351 days show years leap year
+		assertEquals("2016",
+				datesHelper.generateBalanceSheetHeading("2015-04-01T00:00:00.000Z", "2016-03-16T00:00:00.000Z", false));
+
+		// Test 1st year filing within 15 days either side of 1 year period with same
+		// year is just year
+		assertEquals("30 June 2015",
+				datesHelper.generateBalanceSheetHeading("2014-06-01T00:00:00.000Z", "2015-06-30T00:00:00.000Z", true));
+
+	}
+}
