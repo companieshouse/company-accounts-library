40e8dd8bb23b318544f5577faee67f1744304805
Rafa <rgarcia@companieshouse.gov.uk>
2020-04-06 15:37:39 +0100

    Remove Spring stereotype annotations

--- src/main/java/uk/gov/companieshouse/accountsdates/AccountsDatesHelper.java
+++ src/main/java/uk/gov/companieshouse/accountsdates/AccountsDatesHelper.java
@@ -7,9 +7,6 @@ import java.util.Date;
 import java.util.List;
 import java.util.Map;
 
-import org.springframework.stereotype.Service;
-
-@Service
 public interface AccountsDatesHelper {
     
     /**
